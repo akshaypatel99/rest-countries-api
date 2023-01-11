b854be8b0780bc17e56fd68193072b6dad6e0568
akshaypatel99 <akshaypatel99@gmail.com>
2023-01-11 18:50:50 +0000

    Add Error page styles

--- src/components/error/ErrorPage.tsx
+++ src/components/error/ErrorPage.tsx
@@ -1,4 +1,6 @@
-import { useRouteError } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
+import styles from './ErrorPage.module.css';
+import { IoArrowBackSharp } from 'react-icons/io5';
 
 const ErrorPage = () => {
 	const error = useRouteError();
@@ -14,10 +16,13 @@ const ErrorPage = () => {
 	}
 
 	return (
-		<div id='error-page'>
+		<div id='error-page' className={styles.error_page}>
 			<h1>Oops!</h1>
 			<p>Sorry, an unexpected error has occurred.</p>
 			{errorMessage}
+			<Link to='/' className={styles.back_button}>
+				<IoArrowBackSharp /> Back
+			</Link>
 		</div>
 	);
 };
