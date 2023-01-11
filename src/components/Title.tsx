a8eea8ecbc7f44321564b2f4c585500b124689d2
akshaypatel99 <akshaypatel99@gmail.com>
2023-01-11 17:47:22 +0000

    Adjust title size

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ import styles from './Title.module.css';
 const Title = () => {
 	return (
 		<header className={styles.primary_header}>
-			<h1>Where in the world?</h1>
+			<h1 className={styles.title}>Where in the world?</h1>
 			<Switch />
 		</header>
 	);
