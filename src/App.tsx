5d196ef3aa354a1734694079c3eb0273d9a7001c
akshaypatel99 <akshaypatel99@gmail.com>
2023-01-11 11:08:18 +0000

    Lift React Query from App to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
-import { useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { Outlet } from 'react-router-dom';
 import './App.css';
 import Title from './components/Title';
-import Countries from './components/Countries';
-
-const queryClient = new QueryClient();
 
 function App() {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<div className='App'>
-				<Title />
-				<Countries />
-			</div>
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<div className='App'>
+			<Title />
+			<Outlet />
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import './index.css';
 import ErrorPage from './components/error/ErrorPage';
 import DetailPage from './components/detail/DetailPage';
+import Countries from './components/Countries';
+import { loader as countriesLoader } from './hooks/useCountries';
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
 		errorElement: <ErrorPage />,
-	},
-	{
-		path: 'country/:countryId',
-		element: <DetailPage />,
+		children: [
+			{
+				path: '/',
+				element: <Countries />,
+				loader: countriesLoader(queryClient),
+			},
+			{
+				path: 'country/:countryId',
+				element: <DetailPage />,
+			},
+		],
 	},
 ]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<RouterProvider router={router} />
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
